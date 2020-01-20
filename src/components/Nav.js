beaf0cdff48554c75e188fd21d5e2e83d9afdd93
lavinbrenna <53067932+lavinbrenna@users.noreply.github.com>
2020-01-19 19:54:18 -0800

    Add files via upload

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import {BrowserRouter, NavLink} from 'react-router-dom';
 
-const Nav =(props)=>  {
+const Nav =(z)=>  {
 
     return(
   <BrowserRouter>
       <nav className = "main-nav">
        <ul>
            <li><NavLink exact to ='/' >Home</NavLink></li>
-           <li><NavLink to="/keithsonnier" >Keith Sonnier</NavLink></li>
-           <li><NavLink to="/jamesturrell" >James Turrell</NavLink></li>
-           <li><NavLink to="/olafureliasson" >Olafur Eliasson</NavLink></li>
+           <li><NavLink to="/keith" >Keith Sonnier</NavLink></li>
+           <li><NavLink to="/james" >James Turrell</NavLink></li>
+           <li><NavLink to="/olafur" >Olafur Eliasson</NavLink></li>
            <li><NavLink to ="/teamlab">teamLab</NavLink></li>
        </ul>   
       </nav>
